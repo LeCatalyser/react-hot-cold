75a9151ec56e71fb1962fc53865869539cd77837
Luisa Salcedo <profe.salcedo@gmail.com>
2017-10-14 21:37:38 -0500

    created actions and store components

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+//guessing numbers
+export const GUESS_NUMBER = "GUESS_NUMBER";
+export const guessNumber = number => ({
+  type: GUESS_NUMBER,
+  number
+});
+//starting a new game
+
+export const NEW_GAME = "NEW_GAME";
+export const newGame = () => ({
+  type: NEW_GAME
+});
